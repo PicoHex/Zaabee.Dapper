4d0c54190b6cf857c9d32a5bf03a8cf4dc04506b
Mutuduxf <aeondxf@live.com>
2019-05-31 15:09:01 +0800

    Develop

--- Zaabee.Dapper.Repository/Zaabee.Dapper.Repository/ZaabeeRepository.cs
+++ Zaabee.Dapper.Repository/Zaabee.Dapper.Repository/ZaabeeRepository.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Threading.Tasks;
-using Dapper;
 using Zaabee.Dapper.Repository.Abstractions;
 
 namespace Zaabee.Dapper.Repository
